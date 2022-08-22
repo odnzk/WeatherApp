e31f63c2490553cd595f7f507a1fbb3d1a9bf27f
Odenezhkina <66617436+Odenezhkina@users.noreply.github.com>
2022-08-21 20:09:47 -0700

    Design has been improved. Fields like "feels like", "sunset", "sunrise" have been added, main color has been changed

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -21,6 +21,8 @@ import com.example.weatherapp.databinding.FragmentMainBinding
 import com.example.weatherapp.util.WeatherForecastAdapter
 import com.example.weatherapp.util.managers.ConvertingManager
 import com.example.weatherapp.util.managers.LocationManager
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -74,7 +76,7 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun showProgressBar() {
+    fun showProgressBar() {
         for (view in binding.mainCl.children) {
             view.visibility = View.GONE
             if (view.id == R.id.progress_bar) {
@@ -100,13 +102,18 @@ class MainFragment : Fragment() {
             val converter = ConvertingManager(resources)
             weatherForecast.list[0].run {
                 with(main) {
-                    tvTemperature.text = getString(R.string.temperature_unit, temp.toInt())
+                    tvTemperature.text = getString(R.string.temperature_unit, temp.roundToInt())
                     tvPressure.text = getString(R.string.pressure_unit, pressure)
                     tvHumidity.text = getString(R.string.humidity_unit, humidity)
+                    tvFeelsLike.text = getString(R.string.feels_like, feels_like.roundToInt())
                 }
                 tvVisibility.text = converter.convertVisibility(visibility)
                 tvWind.text = getString(R.string.wind_unit, converter.formatDouble(wind.speed))
                 ivWeather.setImageResource(converter.convertIcon(weather[0].id, weather[0].icon))
+            }
+            weatherForecast.city.run{
+                tvSunrise.text = converter.convertTime(sunrise.toLong())
+                tvSunset.text = converter.convertTime(sunset.toLong())
             }
             rvWeather.adapter = WeatherForecastAdapter(weatherForecast.list.drop(1), resources)
         }
